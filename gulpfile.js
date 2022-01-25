e382cc144f8ace0292135192f5dcefb5f5d5ecd5
Etarasevych <tarasevych20@gmail.com>
2022-01-25 18:22:35 +0200

    modified project tree, modified gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('gulp-autoprefixer');
 const htmlmin = require('gulp-htmlmin');
 const cleanCSS = require('gulp-clean-css');
